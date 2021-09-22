070e1e0908a66f8927558fa23cabf2ec6633f4fe
newinfostudio <90056078+newinfostudio@users.noreply.github.com>
2021-09-22 15:48:29 -0400

    Update content.js

--- content/content.js
+++ content/content.js
@@ -133,7 +133,7 @@ export default {
     },
     {
       word: 'Stefanie Tam. ',
-      definition: 'Stefanie Tam is a designer at New Information. She previously worked at Studio Lin, Wax Studios, and the Underground Museum. Stefanie holds a BA in Design, Media Arts from UCLA and attended Yale School of Art’s Graphic Design MFA program. On sunny days, she can be found lying on soft grass patches.'
+      definition: 'Stefanie Tam is a designer at New Information. She previously worked at Studio Lin, Wax Studios, and the Underground Museum. Stefanie holds a BA from UCLA’s Design Media Arts. On sunny days, she can be found lying on soft patches of grass.'
     },
     
     {
