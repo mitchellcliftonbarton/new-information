9352e1aea7d31824ba23891d28fff941f2dfe46f
newinfostudio <90056078+newinfostudio@users.noreply.github.com>
2022-09-23 11:45:35 -0400

    Update projects.js

--- content/projects.js
+++ content/projects.js
@@ -1,7 +1,7 @@
 export default [
   {
     title: 'Year of Uncertainty',
-    caption: 'Caption goes here.',
+    caption: 'Year of Uncertainty (YoU) is a framework for strengthening connection among the Queens Museum, our communities, and constituents, focused on creating new possibilities for culture, kinship, and mutual support.',
     image: 'you.jpg',
     imageAlt: 'Alt text goes here.',
     color: '#FF00C7',
@@ -9,7 +9,7 @@ export default [
   },
   {
     title: 'Listening To Clay',
-    caption: 'Caption goes here.',
+    caption: 'Listening to Clay investigates the work of sixteen Japanese ceramic artists through a dynamic interview format.',
     image: 'NewInformation_ListeningToClay_a.jpg',
     imageAlt: 'Alt text goes here.',
     color: '#0038FF',
@@ -17,7 +17,7 @@ export default [
   },
   {
     title: 'Graduate Programs Spitzer Unit System',
-    caption: 'Caption goes here.',
+    caption: 'Our website for the Spitzer Unit System surfaces relationships among students, faculty, and their respective courses of study.',
     image: 'NewInformation_Spitzer_a.jpg',
     imageAlt: 'Alt text goes here.',
     color: '#FFCE00',
@@ -25,19 +25,27 @@ export default [
   },
   {
     title: 'All The Small Things',
-    caption: 'Caption goes here.',
+    caption: 'All The Small Things, takes the reader on a dynamic tour of Cory Arcangel’s museum installation, interweaving texts, code, and musical charts along the way.',
     image: 'NewInformation_AllTheSmallThings_a.jpg',
     imageAlt: 'Alt text goes here.',
     color: '#1BFA33',
     slug: 'all-the-small-things'
   },
   {
-    title: 'The Elm',
-    caption: 'Caption goes here.',
-    image: 'NewInformation_TheElm_a.jpg',
+    title: 'Nike KD 14',
+    caption: 'Branding for Nike’s KD14 exuberantly conveys the energy of both the athlete and his footwear.',
+    image: 'NewInformation_NikeKD14_a.jpg',
     imageAlt: 'Alt text goes here.',
-    color: '#008E20',
-    slug: 'the-elm'
+    color: '#1BFA33',
+    slug: 'nike-kd-14'
+  },
+  {
+    title: 'Krink',
+    caption: 'An e-commerce site for Krink features a specimen-like grid, oversized filtering tools, and a graffiti-based screensaver.',
+    image: 'NewInformation_Krink_a.jpg',
+    imageAlt: 'Alt text goes here.',
+    color: '#1BFA33',
+    slug: 'nike-kd-14'
   },
   {
     title: 'Agency—Agency',
@@ -52,7 +60,13 @@ export default [
     video: 'NewInformation_Sacha Maric_a.mp4',
     color: '#00C9FF',
     slug: 'sacha-maric'
+  },
+  {
+    title: 'The Elm',
+    caption: 'Identity for a new restaurant in the Berkshires featuring contemporary New England style cuisine.',
+    image: 'NewInformation_TheElm_a.jpg',
+    imageAlt: 'Alt text goes here.',
+    color: '#008E20',
+    slug: 'the-elm'
   }
- 
-  
 ]
