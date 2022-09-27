e2efb74a61105898a73b3be03bf291eaa9c80dda
newinfostudio <90056078+newinfostudio@users.noreply.github.com>
2022-09-27 12:52:30 -0400

    Update projects.js

--- content/projects.js
+++ content/projects.js
@@ -1,14 +1,14 @@
 export default [
   {
-    title: 'Year of Uncertainty',
+    title: 'with Queens Museum',
     caption: 'Exhibition design surfacing a year of conversation  and experimentation at the Queens Museum.',
     image: 'NewInformation_YoUExhibition_a.jpg',
     imageAlt: 'Alt text goes here.',
-    color: '#e89751',
+    color: '#000000',
     slug: 'year-of-uncertainty'
   },
   {
-    title: 'Year of Uncertainty',
+    title: 'with Queens Museum',
     caption: 'A collaborative digital platform surfacing a year of conversation  and experimentation at the Queens Museum.',
     image: 'NewInformation_YoUWebsite_a.jpg',
     imageAlt: 'Alt text goes here.',
@@ -16,7 +16,7 @@ export default [
     slug: 'year-of-uncertainty-website'
   },
   {
-    title: 'Listening to Clay',
+    title: 'with Monacelli Press',
     caption: 'Listening to Clay investigates the work of sixteen Japanese ceramic artists through a dynamic interview format.',
     image: 'NewInformation_ListeningToClay_a.jpg',
     imageAlt: 'Alt text goes here.',
@@ -24,7 +24,7 @@ export default [
     slug: 'listening-to-clay'
   },
   {
-    title: 'Spitzer Unit System',
+    title: 'with Spitzer Architecture',
     caption: 'Website for graduate architecture programs surfaces relationships among students, faculty, and their respective courses of study.',
     image: 'NewInformation_Spitzer_a.jpg',
     imageAlt: 'Alt text goes here.',
@@ -32,7 +32,7 @@ export default [
     slug: 'graduate-programs-spitzer-unit-system'
   },
   {
-    title: 'Nike KD14',
+    title: 'with Nike',
     caption: 'Branding for Nike’s KD14 captures the energy of both the athlete and his footwear.',
     image: 'NewInformation_NikeKD14_a.jpg',
     imageAlt: 'Alt text goes here.',
@@ -40,7 +40,7 @@ export default [
     slug: 'nike-kd-14'
   },
   {
-    title: 'All The Small Things',
+    title: 'with Cory Arcangel',
     caption: 'All The Small Things takes the reader on a dynamic tour of Cory Arcangel’s Heart Museum installation.',
     image: 'NewInformation_AllTheSmallThings_a.jpg',
     imageAlt: 'Alt text goes here.',
@@ -48,7 +48,7 @@ export default [
     slug: 'all-the-small-things'
   },
   {
-    title: 'Krink',
+    title: 'with Krink',
     caption: 'E-commerce site for Krink featuring a specimen-like grid, oversized filtering tools, and a graffiti-based screensaver.',
     image: 'NewInformation_Krink_a.jpg',
     imageAlt: 'Alt text goes here.',
@@ -56,7 +56,7 @@ export default [
     slug: 'krink'
   },
   {
-    title: 'Queens Museum',
+    title: 'with Queens Museum',
     caption: 'A new brand identity for the Queens Museum connects past with present through a flexible conduit and custom typeface.',
     image: 'NewInformation_QueensMuseum_a.jpg',
     imageAlt: 'Image of an entrance doorway to the Queens Museum featuring bright green entrance signage in both English and Spanish.',
@@ -64,14 +64,14 @@ export default [
     slug: 'queens-museum'
   },
   {
-    title: 'Jeremy Liebman',
+    title: 'with Jeremy Liebman',
     caption: 'Search-based website cataloging the prolific works of Jeremy Liebman.',
     video: 'NewInformation_JeremyLiebman_a.mp4',
     color: '#FFFF00',
     slug: 'jeremy-liebman'
   },
   {
-    title: 'The Elm',
+    title: 'with The Elm',
     caption: 'Identity for a new restaurant in the Berkshires offering contemporary New England style cuisine.',
     image: 'NewInformation_TheElm_a.jpg',
     imageAlt: 'Alt text goes here.',
@@ -79,14 +79,14 @@ export default [
     slug: 'the-elm'
   },
   {
-    title: 'Agency—Agency',
+    title: 'with Agency—Agency',
     caption: 'Portfolio site for the evolving, investigative design practice of Tei Carpenter.',
     video: 'NewInformation_AgencyAgency_a.mp4',
     color: '#FF0000',
     slug: 'agency-agency'
   },
   {
-    title: 'Sacha Maric',
+    title: 'with Sacha Maric',
     caption: 'Portfolio site for the elastic fashion photographer Sacha Maric.',
     video: 'NewInformation_Sacha Maric_a.mp4',
     color: '#00FFFF',
