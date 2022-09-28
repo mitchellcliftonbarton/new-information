ffd3a2e143fab5e5586f832b3aef21ee53d75d52
stefanietam <stefanie.e.tam@gmail.com>
2022-09-28 16:12:02 -0400

    Update information.js

--- content/information.js
+++ content/information.js
@@ -1,9 +1,8 @@
 export default {
   address: '67 West St<br>Unit 406<br>Brooklyn, NY<br>11222',
-  addressLink: 'https://www.google.com/maps/place/67+West+St+%23426,+Brooklyn,+NY+11222/@40.728737,-73.9621379,17z/data=!3m1!4b1!4m5!3m4!1s0x89c2596a71410297:0x4576c6466e60fd74!8m2!3d40.728737!4d-73.9599492',
   email: 'hello@newinfo.studio',
   emailString: 'hello@<wbr>newinfo.studio',
   instagram: 'https://www.instagram.com/newinfo.studio/',
   textColumn1: 'A Brooklyn based graphic design studio that builds community through typography and technology. New Information sees design as an opportunity to imagine and enable connections between people, ideas, and objects in the world.<br><br>Services<br>Art Direction<br>Creative Direction<br>Identity Design<br>Motion Design<br>Exhibition Design<br>Signage Design<br>Publication Design<br>Web Design.<br><br>Selected Clients<br>Agency—Agency<br>Asian American Arts Alliance<br>The Bernard & Anne Spitzer School of Architecture<br>Emerson Collective<br>Krink<br>Mastic Beach Conservancy<br>Monacelli Press<br>Nike<br>Processing Foundation<br>Queens Museum<br>Two Palms<br>Urban Outfitters',
-  textColumn2: 'Dave Yun is the founder and creative director of New Information. He formerly ran Wax Studios, Wax Magazine, Wax Radio, and led teams at 2x4 and Pentagram. Dave has taught and critiqued at Yale School of Art, Columbia University, and Parsons, and holds an MFA from Yale School of Art.<br><br>Designers<br>Inyeong Cho<br>Stefanie Tam<br><br>Collaborator<a href="https://cold-rice.info/" target="_blank">Cold Rice</a><br><a href="https://kellenrenstrom.com/" target="_blank">Kellen Renstrom.</a>'
+  textColumn2: 'Dave Yun is the founder and creative director of New Information. He formerly ran Wax Studios, Wax Magazine, Wax Radio, and led teams at 2x4 and Pentagram. Dave has taught and critiqued at Yale School of Art, Columbia University, and Parsons, and holds an MFA from Yale School of Art.<br><br>Designers<br>Inyeong Cho<br>Stefanie Tam<br><br>Collaborators<br><a href="https://cold-rice.info/" target="_blank">Cold Rice</a><br><a href="https://kellenrenstrom.com/" target="_blank">Kellen Renstrom</a>'
 }
