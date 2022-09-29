e848f38ad56b0c9fe1dd095dffbee58e2d33fa67
newinfostudio <90056078+newinfostudio@users.noreply.github.com>
2022-09-29 17:55:26 -0400

    Update projects.js

--- content/projects.js
+++ content/projects.js
@@ -4,7 +4,7 @@ export default [
     caption: 'Exhibition design for Year of Uncertainty (YoU), a framework for strengthening connections among the Queens Museum and its communities.',
     image: 'NewInformation_YoUExhibition_a.jpg',
     imageAlt: 'Alt text goes here.',
-    color: '#ff6000',
+    color: '#ff8400',
     slug: 'year-of-uncertainty'
   },
   {
