ed7821cdd55886d089b619bc04600d8f8f592062
newinfostudio <90056078+newinfostudio@users.noreply.github.com>
2022-09-30 11:14:20 -0400

    Update projects.js

--- content/projects.js
+++ content/projects.js
@@ -1,7 +1,7 @@
 export default [
   {
     title: 'Queens Museum',
-    caption: 'Exhibition design for Year of Uncertainty (YoU), a framework for strengthening connections among the Queens Museum and its communities.',
+    caption: 'Exhibition design for Year of Uncertainty (YoU), a framework that strengthens connections between the Queens Museum and its communities.',
     image: 'NewInformation_YoUExhibition_a.jpg',
     imageAlt: 'Alt text goes here.',
     color: '#fe8c14',
@@ -9,7 +9,7 @@ export default [
   },
   {
     title: 'Queens Museum',
-    caption: 'A collaborative digital platform for Year of Uncertainty (YoU), investigating and strengthening connections among the Queens Museum and its communities. View <a href="https://you.queensmuseum.org/" target="_blank">here.</a>',
+    caption: 'Website design for Year of Uncertainty (YoU), a digital platform that investigates and strengthens connections between the Queens Museum and its communities. View <a href="https://you.queensmuseum.org/" target="_blank">here.</a>',
     image: 'NewInformation_YoUWebsite_a.jpg',
     imageAlt: 'Alt text goes here.',
     mobileImage: 'NewInformation_YoUWebsite_Mobile_a.jpg',
@@ -18,7 +18,7 @@ export default [
   },
   {
     title: 'Monacelli Press',
-    caption: 'Listening to Clay investigates the work of sixteen Japanese ceramic artists through a dynamic interview format.',
+    caption: 'Publication design for Listening to Clay, an investigation of the work of sixteen Japanese ceramic artists through a dynamic interview format.',
 /*    video: 'NewInformation_ListeningToClay_b.mp4', */
     image: 'NewInformation_ListeningToClay_a.jpg',
     imageAlt: 'Alt text goes here.',
@@ -28,7 +28,7 @@ export default [
   },
   {
     title: 'Spitzer Architecture',
-    caption: 'Website for graduate architecture programs surfaces relationships among students, faculty, and their respective courses of study.',
+    caption: 'Website design for Spitzer Architecture’s Unit System, a graduate program that surfaces relationships between students, faculty, and their respective courses of study.',
     video: 'NewInformation_Spitzer_a.mp4',
     imageAlt: 'Alt text goes here.',
 /*    color: '#FFCE00', */
@@ -37,7 +37,7 @@ export default [
   },
   {
     title: 'Nike',
-    caption: 'Branding for Nike’s KD14 captures the energy of both the athlete and the footwear.',
+    caption: 'Brand design for Nike’s KD14, a campaign that captures the energy of both the athlete and the footwear.',
     image: 'NewInformation_NikeKD14_a.jpg',
     imageAlt: 'Alt text goes here.',
     color: '#00ffff',
@@ -45,7 +45,7 @@ export default [
   },
   {
     title: 'Cory Arcangel',
-    caption: 'All The Small Things takes the reader on a dynamic tour of Cory Arcangel’s Heart Museum installation.',
+    caption: 'Publication design for All The Small Things, a dynamic tour of Cory Arcangel’s Heart Museum installation.',
     image: 'NewInformation_AllTheSmallThings_a.jpg',
     imageAlt: 'Alt text goes here.',
     mobileImage: 'NewInformation_AllTheSmallThings_Mobile_a.jpg',
@@ -54,7 +54,7 @@ export default [
   },
   {
     title: 'Krink',
-    caption: 'E-commerce site for Krink featuring a specimen-like grid, oversized filtering tools, and a graffiti-based screensaver.',
+    caption: 'E-commerce website design for Krink, an artist materials brand and creative studio that features a specimen-like grid, oversized filtering tools, and a graffiti-based screensaver.',
     image: 'NewInformation_Krink_a.jpg',
     imageAlt: 'Alt text goes here.',
     mobileImage: 'NewInformation_Krink_Mobile_a.jpg',
@@ -63,7 +63,7 @@ export default [
   },
   {
     title: 'Processing Foundation',
-    caption: 'The Processing Community Catalog is a 900 page tome that documents the activities of the Processing Foundation from the last 20 years.',
+    caption: 'Publication design for the Processing Community Catalog, a 900 page tome that documents the activities of the Processing Foundation from the last 20 years.',
     image: 'NewInformation_ProcessingFoundation_a.jpg',
     imageAlt: 'Alt text goes here.',
     mobileImage: 'NewInformation_ProcessingFoundation_Mobile_a.jpg',
@@ -72,7 +72,7 @@ export default [
   },
   {
     title: 'The Elm',
-    caption: 'Identity for a new restaurant in the Berkshires offering contemporary New England style cuisine.',
+    caption: 'Identity design for The Elm, a new restaurant in the Berkshires that offers contemporary New England style cuisine.',
     image: 'NewInformation_TheElm_a.jpg',
     imageAlt: 'Alt text goes here.',
     color: '#0b5714',
@@ -81,14 +81,14 @@ export default [
 
   {
     title: 'Jeremy Liebman',
-    caption: 'A search-based website cataloging the prolific works of Jeremy Liebman.',
+    caption: 'Search-based website design for Jeremy Liebman that catalogs his prolific work.',
     video: 'NewInformation_JeremyLiebman_a.mp4',
     color: '#FFFF00',
     slug: 'jeremy-liebman'
   },
   {
     title: 'Queens Museum',
-    caption: 'A new brand identity for the Queens Museum connects past with present through a flexible conduit and custom typeface.',
+    caption: 'Brand identity for the Queens Museum, an institution that connects the past with the present through a flexible conduit and custom typeface.',
     image: 'NewInformation_QueensMuseum_a.jpg',
     imageAlt: 'Image of an entrance doorway to the Queens Museum featuring bright green entrance signage in both English and Spanish.',
     color: '#84ff00',
@@ -96,14 +96,14 @@ export default [
   },
   {
     title: 'Agency—Agency',
-    caption: 'A temporary site for the evolving, exuberant, investigative design practice of Tei Carpenter.',
+    caption: 'Temporary website design for Agency—Agency, an evolving, exuberant, investigative architectural design practice by Tei Carpenter.',
     video: 'NewInformation_AgencyAgency_a.mp4',
     color: '#ff2830',
     slug: 'agency-agency'
   },
   {
     title: 'Sacha Maric',
-    caption: 'A portfolio website for Sacha Maric fluidly traverses fashion, personal, and portrait photography.',
+    caption: 'Portfolio website design for Sacha Maric, a photographer who’s work fluidly traverses fashion, personal, and portraiture.',
     video: 'NewInformation_SachaMaric_a.mp4',
     color: '#ff99e1',
     slug: 'sacha-maric'
