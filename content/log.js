d49a0b5069acd2183a8bcac75916a81d3a758139
newinfostudio <90056078+newinfostudio@users.noreply.github.com>
2022-10-05 11:00:37 -0400

    Update log.js

--- content/log.js
+++ content/log.js
@@ -1,7 +1,7 @@
 export default [
   {
     date: '10.05.22',
-    text: 'has been listening to music by Shin Joong Hyun 신중현, Thundercat, Jenevieve, and more.'
+    text: 'has been listening to music by Jenevieve, Shin Joong Hyun 신중현, Thundercat, and more.'
   },
   {
     date: '10.05.22',
