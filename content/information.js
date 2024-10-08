99d51be166a7e75fe0917c47991df1dfbb51f747
Mitchell Barton <mitchell@welikesmall.com>
2024-10-08 12:55:44 -0600

    updated info

--- content/information.js
+++ content/information.js
@@ -1,57 +1,10 @@
 export default {
-  address: '42 West St<br>Suite 339<br>Brooklyn, NY<br>11222',
-  email: 'hello@newinfo.studio',
-  emailString: 'hello@<wbr>newinfo.studio',
-  instagram: 'https://www.instagram.com/newinfo.studio/',
-  textColumn1: 'A Brooklyn based graphic design studio that builds community through typography and technology. New Information sees design as an opportunity to imagine and enable connections between people, ideas, and objects in the world.<br><br>Services<br>Brand Strategy<br>Brand Identity Design<br>Web Design<br>Art Direction<br>Creative Direction<br>Motion Design<br>Exhibition Design<br>Signage Design<br>Publication Design<br><br> 
-Current & Past Clients<br>
-Agency—Agency<br>
-AIA New York<br>
-American Literary Translators Association<br>
-Arper<br>
-Artsy<br>
-Asian American Arts Alliance<br>
-Atrbute<br>
-Cory Arcangel<br>
-Emerson Collective<br>
-Gap<br>
-Google<br>
-Krink<br>
-Levis<br>
-Mastic Beach Conservancy<br>
-Mexican Summer<br>
-Moda Operandi<br>
-MoMA<br>
-Monacelli Press<br>
-Nike<br>
-Patagonia<br>
-PennDesign<br>
-Peterson Rich Office<br>
-Processing Foundation<br>
-Queens Museum<br>
-Rubin Museum of Art<br>
-Spitzer School of Architecture<br>
-Storefront for Art & Architecture<br>
-Teva<br>
-Two Palms<br>
-Urban Outfitters<br>
-Van Alen Insitute<br>
-vowels<br><br><br>',
-
-textColumn2: 'New Information was founded in 2021 by Dave Yun, out of a desire to create deep and meaningful partnerships with organizations involved in arts, design and community building, and to bring a new approach to the traditional agency model.
-<br><br>
-Our studio is dynamic, pairing the right team members to the right client, ensuring tailored project outcomes delivered at the highest standard. Founded in Brooklyn, our team now spans multiple cities, states and timezones. Prior to founding New Information, Dave built Wax Studios, where he led  design and technology projects across the arts and cultural landscape in the US and Internationally, including MoMA, Google, Nike, Rubin Museum of Art, Artsy, Asian American Arts Alliance, Kunsthall Stavanger, Anton Kern Gallery, Herning Museum of Art, Luhring Augustine Gallery, and PennDesign. 
-<br><br>
-Collaborators<br>
-<a href="https://cold-rice.info/" target="_blank">⭢ Cold Rice</a><br>
-⭢ Dexin Leah Chen<br>
-⭢ Eddie Dohyun Kim<br>
-⭢ Hunni Dev<br>
-⭢ Inyeong Cho<br>
-⭢ Jaiwon Lee<br>
-<a href="https://kellenrenstrom.com/" target="_blank">⭢ Kellen Renstrom</a><br>
-⭢ Min Hee Lee<br>
-⭢ Sacha Maric<br>
-⭢ Scott Haven<br>
-⭢ Stefanie Tam<br>'
-}
+  address: "42 West St<br>Suite 339<br>Brooklyn, NY<br>11222",
+  email: "hello@newinfo.studio",
+  emailString: "hello@<wbr>newinfo.studio",
+  instagram: "https://www.instagram.com/newinfo.studio/",
+  textColumn1:
+    "A Brooklyn based graphic design studio that builds community through typography and technology. New Information sees design as an opportunity to imagine and enable connections between people, ideas, and objects in the world.<br><br>Services<br>Brand Strategy<br>Brand Identity Design<br>Web Design<br>Art Direction<br>Creative Direction<br>Motion Design<br>Exhibition Design<br>Signage Design<br>Publication Design<br><br> Current & Past Clients<br>Agency—Agency<br>AIA New York<br>American Literary Translators Association<br>Arper<br>Artsy<br>Asian American Arts Alliance<br>Atrbute<br>Cory Arcangel<br>Emerson Collective<br>Gap<br>Google<br>Krink<br>Levis<br>Mastic Beach Conservancy<br>Mexican Summer<br>Moda Operandi<br>MoMA<br>Monacelli Press<br>Nike<br>Patagonia<br>PennDesign<br>Peterson Rich Office<br>Processing Foundation<br>Queens Museum<br>Rubin Museum of Art<br>Spitzer School of Architecture<br>Storefront for Art & Architecture<br>Teva<br>Two Palms<br>Urban Outfitters<br>Van Alen Insitute<br>vowels<br><br><br>",
+  textColumn2:
+    'New Information was founded in 2021 by Dave Yun, out of a desire to create deep and meaningful partnerships with organizations involved in arts, design and community building, and to bring a new approach to the traditional agency model.<br><br>Our studio is dynamic, pairing the right team members to the right client, ensuring tailored project outcomes delivered at the highest standard. Founded in Brooklyn, our team now spans multiple cities, states and timezones. Prior to founding New Information, Dave built Wax Studios, where he led  design and technology projects across the arts and cultural landscape in the US and Internationally, including MoMA, Google, Nike, Rubin Museum of Art, Artsy, Asian American Arts Alliance, Kunsthall Stavanger, Anton Kern Gallery, Herning Museum of Art, Luhring Augustine Gallery, and PennDesign. <br><br>Collaborators<br><a href="https://cold-rice.info/" target="_blank">⭢ Cold Rice</a><br>⭢ Dexin Leah Chen<br>⭢ Eddie Dohyun Kim<br>⭢ Hunni Dev<br>⭢ Inyeong Cho<br>⭢ Jaiwon Lee<br><a href="https://kellenrenstrom.com/" target="_blank">⭢ Kellen Renstrom</a><br>⭢ Min Hee Lee<br>⭢ Sacha Maric<br>⭢ Scott Haven<br>⭢ Stefanie Tam<br>'
+};
