7bec147bf443a5a34cc9e33bc1f481d13a0d705b
newinfostudio <90056078+newinfostudio@users.noreply.github.com>
2025-10-14 16:25:03 -0400

    Update seo.js

--- content/seo.js
+++ content/seo.js
@@ -1,6 +1,6 @@
 export default {
   title: 'New Information',
-  description: 'New Information is a Brooklyn based design studio that opened in 2021. Currently, we are working with the Bernard and Anne Spitzer School of Architecture, the Queens Museum, Monacelli Press, and the Processing Foundation. We are Dave Yun, Inyeong Cho, and Stefanie Tam. To get in touch, please email us at hello@newinfo.studio or subscribe to our occasional newsletter.',
-  keywords: 'New, Information, Studio, Brooklyn, Design, Agency, Branding, Identity, Brand Identity, Website, Graphics, Mobile, UI, UX, Books, Catalogs, Strategy',
+  description: 'New Information is an award-winning design studio partnering with culturally-forward institutions and brands. To get in touch, please email us at hello@newinfo.studio.',
+  keywords: 'New, Information, Info, Studio, Brooklyn, Design, Agency, Branding, Identity, Brand Identity, Website, Web, Graphic, Graphics, Mobile, UI, UX, Books, Catalogs, Strategy',
   image: '/NewInformation_Profile.jpg' // this needs the "/" before it, and should live in the 'static' directory
 }
